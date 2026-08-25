fbc391a2bba47df3148962605a50a10fd1fe20fe
Aman Sharma <amansharma19696@gmail.com>
2026-08-25 23:06:01 +0530

    Add market data to order book integration test

---
+++ tests/market_data/order_book_feed_test.cpp
@@ -0,0 +1,30 @@
+#include <gtest/gtest.h>
+
+#include "market_data/simulated_feed.h"
+#include "order_book/order_book.h"
+
+TEST(MarketDataPipelineTest, SimulatedFeedBuildsOrderBook)
+{
+    SimulatedFeed feed;
+    OrderBook book;
+
+    MarketDataEvent event;
+
+    while (feed.next_event(event))
+    {
+        book.apply(event);
+    }
+
+    EXPECT_EQ(book.best_bid(), 10000);
+    EXPECT_EQ(book.best_ask(), 10001);
+
+    EXPECT_EQ(book.bid_quantity(10000), 500);
+    EXPECT_EQ(book.bid_quantity(9999), 300);
+
+    EXPECT_EQ(book.ask_quantity(10001), 200);
+    EXPECT_EQ(book.ask_quantity(10002), 400);
+    EXPECT_EQ(book.ask_quantity(10003), 600);
+
+    EXPECT_EQ(book.spread(), 1);
+    EXPECT_EQ(book.mid_price(), 10000);
+}
